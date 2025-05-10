9a7c9dccc87178e5b12b5e4515579967eb6e82f2
etjen <buljubasicetjen4@gmail.com>
2025-05-10 19:29:16 +0200

    Initial commit, basic spring security, csrf protection

---
+++ src/main/java/org/etjen/spring_sec/HelloController.java
@@ -0,0 +1,18 @@
+package org.etjen.spring_sec;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(){
+        return "Hello World";
+    }
+
+    @GetMapping("about")
+    public String about(){
+        return "etjen";
+    }
+}
